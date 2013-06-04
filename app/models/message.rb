85f02727744dfc3115119f5bb54d5bb1b3ccbef1
hiro-nakaji <hiro-nakaji@wf7.so-net.ne.jp>
2013-06-04 22:52:13 +0900

    Modified return path on thread page.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,14 @@
 class Message < ActiveRecord::Base
   scope :desc, -> { order('created_at DESC') }
+  scope :newer, ->(created_at) { where('created_at >= ?', created_at) }
 
   include Entry
   extend EntryExtend
 
   has_many :comments, dependent: :destroy, order: :created_at
+
+  def current_page
+    count = Message.newer(self.created_at).count
+    (count.to_f / Kaminari.config.default_per_page).ceil
+  end
 end
