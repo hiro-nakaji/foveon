e1574d0cf96043d5d10024221e529079bda0e1af
hiro-nakaji <hiro-nakaji@wf7.so-net.ne.jp>
2013-06-07 01:57:21 +0900

    Moved some scopes to EntryExtend.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,4 @@ class Comment < ActiveRecord::Base
   extend EntryExtend
 
   belongs_to :message
-
-  scope :search_title, ->(word) { where(arel_table[:title].matches word) }
-  scope :search_author, ->(word) { where(arel_table[:author].matches word) }
-  scope :search_content, ->(word) { where(arel_table[:content].matches word) }
-  scope :search, ->(word){search_title(word).search_author(word).search_content(word)}
-
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -7,11 +7,6 @@ class Message < ActiveRecord::Base
   scope :desc, -> { order(created_at: :desc) }
   scope :newer, ->(created_at) { where('created_at >= ?', created_at) }
 
-  scope :search_title, ->(word) { where(arel_table[:title].matches word) }
-  scope :search_author, ->(word) { where(arel_table[:author].matches word) }
-  scope :search_content, ->(word) { where(arel_table[:content].matches word) }
-  scope :search, ->(word){search_title(word).search_author(word).search_content(word)}
-
   def current_page
     count = Message.newer(self.created_at).count
     (count.to_f / Kaminari.config.default_per_page).ceil
@@ -22,7 +17,7 @@ class Message < ActiveRecord::Base
     comment_select =  Comment.select(:message_id).where(comment_where)
 
     message_where = Message.where_from_input(input)
-    
+
     Message.where(Message.where(message_where).where(id: comment_select).where_values.reduce(:or))
   end
 end
