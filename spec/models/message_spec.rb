95f2da23d812cddc4e6a2504845a28e3ad5bde0f
hiro-nakaji <hiro-nakaji@wf7.so-net.ne.jp>
2013-06-14 16:04:08 +0900

    Add a test to Message#current_page.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -6,17 +6,32 @@ describe Message do
     it { should validate_presence_of(:author) }
     it { should validate_presence_of(:password) }
     it { should_not validate_presence_of(:mail) }
-    it { should allow_value('mackerel-chef@example.com').for(:mail)}
-    it { should_not allow_value('mackerel-chef.@example.com').for(:mail)}
+    it { should allow_value('mackerel-chef@example.com').for(:mail) }
+    it { should_not allow_value('mackerel-chef.@example.com').for(:mail) }
     it { should_not validate_presence_of(:homepage) }
-    it { should allow_value('http://www.asahi.com').for(:homepage)}
-    it { should allow_value('https://www.asahi.com').for(:homepage)}
-    it { should_not allow_value('www.asahi.com').for(:homepage)}
+    it { should allow_value('http://www.asahi.com').for(:homepage) }
+    it { should allow_value('https://www.asahi.com').for(:homepage) }
+    it { should_not allow_value('www.asahi.com').for(:homepage) }
     it { should validate_presence_of(:content) }
     it { should_not validate_presence_of(:remote_addr) }
     it { should_not validate_presence_of(:user_agent) }
 
     it { should_not validate_presence_of(:old_id) }
     it { should_not validate_presence_of(:message_type) }
+  end
+
+  describe "current_page" do
+    let!(:message) { FactoryGirl.create(:message) }
+
+    subject { message }
+
+    before do
+      Kaminari.config.default_per_page = 10
+      relation = mock(ActiveRecord::Relation)
+      relation.should_receive(:count).and_return(101)
+      Message.should_receive(:newer).and_return(relation)
+    end
+
+    its(:current_page) { should == 11 }
   end
 end
