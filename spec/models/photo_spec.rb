0b6becb6846552c9c5524696e07b3762b5b4783a
hiro-nakaji <hiro-nakaji@wf7.so-net.ne.jp>
2013-06-14 22:59:24 +0900

    Remove tests abount polymorphic attributes.

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe Photo do
   describe "validations" do
-    it { should validate_presence_of(:entry_id) }
-    it { should validate_presence_of(:entry_type) }
     it { should_not validate_presence_of(:title) }
     it { should validate_presence_of(:no) }
     it { should validate_presence_of(:photo_data) }
