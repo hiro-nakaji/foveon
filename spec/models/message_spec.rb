1458a6d881c36bc197724bb74da4943da9d18572
hiro-nakaji <hiro-nakaji@wf7.so-net.ne.jp>
2013-06-14 16:44:26 +0900

    Add tests to Message#new_entry?

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -27,11 +27,41 @@ describe Message do
 
     before do
       Kaminari.config.default_per_page = 10
-      relation = mock(ActiveRecord::Relation)
+      relation                         = mock(ActiveRecord::Relation)
       relation.should_receive(:count).and_return(101)
       Message.should_receive(:newer).and_return(relation)
     end
 
     its(:current_page) { should == 11 }
+  end
+
+  describe "new_entry?" do
+    subject { message }
+
+    let!(:current_time) { Time.now }
+
+    before do
+      Time.stub(:now).and_return(current_time)
+    end
+
+    context "not new entry" do
+      let!(:message) {
+        FactoryGirl.create(:message,
+                           created_at: current_time - 24.hours,
+                           updated_at: current_time - 24.hours)
+      }
+
+      it { should_not be_new_entry }
+    end
+
+    context "new entry" do
+      let!(:message) {
+        FactoryGirl.create(:message,
+                           created_at: current_time - 24.hours + 1.second,
+                           updated_at: current_time - 24.hours + 1.second)
+      }
+
+      it { should be_new_entry }
+    end
   end
 end
