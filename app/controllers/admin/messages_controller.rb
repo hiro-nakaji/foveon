aff63228aac77f6076785332e14a4e06c7f9100e
hiro-nakaji <hiro-nakaji@wf7.so-net.ne.jp>
2014-06-14 22:14:03 +0900

    Implement Admin::MessagesController#bulk_destroy.

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,10 +1,16 @@
 class Admin::MessagesController < Admin::ApplicationController
   def index
+    @messages = Message.desc.page(params[:page]).per(Settings.admin.messages.per_page)
   end
 
   def destroy
   end
 
   def bulk_destroy
+    ActiveRecord::Base.transaction do
+      Message.where(id: params[:messages]).destroy_all
+    end
+
+    redirect_to admin_messages_path
   end
 end
